abba34bab72c96cb336b5d4d4052441933c85d3c
Gerardo Santillan <gerardo.sc.dev@gmail.com>
2026-02-05 20:42:42 -0600

    Add theme for switch light or dark system

--- app/src/main/java/com/example/miminimart/common/ui/colors/AppTheme.kt
+++ app/src/main/java/com/example/miminimart/common/ui/colors/AppTheme.kt
@@ -1,6 +1,8 @@
 package com.example.miminimart.common.ui.colors
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.staticCompositionLocalOf
 
@@ -10,4 +12,15 @@ object AppTheme {
     val colors: AppColors
         @Composable @ReadOnlyComposable
         get() = LocalAppColors.current
+}
+
+@Composable
+fun AppThemeProvider(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColors else LightColors
+    CompositionLocalProvider(LocalAppColors provides colors) {
+        content()
+    }
 }
